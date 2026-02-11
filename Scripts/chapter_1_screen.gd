004c703728c566c523fef6933a01238b39e28435
rileyrowanosborne <rileyrowanosborne@gmail.com>
2026-02-10 20:44:16 -0800

    title and cut-sene updates
    -added a pointlight2D node to the title screen.
    
    -the sprites now rotate at separate speeds.
    
    -added the child to chapter one cutscene.

--- Scripts/chapter_1_screen.gd
+++ Scripts/chapter_1_screen.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ChangeSceneTimer.start(3)
+	$TheBoyTimer.start()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +14,8 @@ func _process(delta: float) -> void:
 
 func _on_change_scene_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/chapter_one.tscn")
+
+
+func _on_the_boy_timer_timeout() -> void:
+	$RichTextLabel.text = "THE CHILD"
+	$ChangeSceneTimer.start()
