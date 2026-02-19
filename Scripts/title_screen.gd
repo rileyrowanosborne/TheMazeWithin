65c2649dd2c91fcb3bae665d7a654cf5d787ebaa
rileyrowanosborne <rileyrowanosborne@gmail.com>
2026-02-18 22:28:20 -0800

    Gorgeous Title Screen Update

---
+++ Scripts/title_screen.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+
+func _ready() -> void:
+	GameState.current_chapter = 1
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/chapter_screen.tscn")
