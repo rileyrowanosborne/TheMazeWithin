828286ce98193c87e6dd375b5365da4b0e3af66f
rileyrowanosborne <rileyrowanosborne@gmail.com>
2026-03-16 18:02:21 -0700

    fuck my life

---
+++ Scripts/buttons 3.gd
@@ -0,0 +1,41 @@
+extends Control
+
+
+
+@onready var resume_button: Button = $ResumeButton
+@onready var options_button: Button = $OptionsButton
+@onready var quit_button: Button = $QuitButton
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalBus.connect("pause_game", on_game_paused)
+	SignalBus.connect("unpause_game",on_game_unpaused)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if resume_button.position.x < -230:
+		resume_button.position.x = 1160
+	
+	if quit_button.position.x < -230:
+		quit_button.position.x = 1160
+	
+	if options_button.position.x < -230:
+		options_button.position.x = 1160
+	
+	
+	resume_button.position.x -= 50 * delta
+	options_button.position.x -= 50 * delta
+	quit_button.position.x -= 50 * delta
+
+
+
+func on_game_paused():
+	resume_button.grab_focus()
+
+func on_game_unpaused():
+	pass
---
+++ Scripts/windows_within_cutscene 3.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+@onready var start_timer: Timer = $StartTimer
+@onready var end_timer: Timer = $EndTimer
+
+@onready var windows: AnimatedSprite2D = $Windows
+@onready var eyes: AnimatedSprite2D = $Eyes
+@onready var main_anims: AnimationPlayer = $MainAnims
+
+
+func _ready() -> void:
+	start_timer.start(1)
+
+
+func _on_start_timer_timeout() -> void:
+	windows.play("default")
+	eyes.play("default")
+
+
+func _on_windows_animation_finished() -> void:
+	main_anims.play("Main")
+	end_timer.start(2)
+	
+
+
+func _on_end_timer_timeout() -> void:
+	pass # Replace with function body.
