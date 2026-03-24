15d955c7439a86eaa0289a00c6e461e1ca4bbf90
rileyrowanosborne <rileyrowanosborne@gmail.com>
2026-03-23 19:21:24 -0700

    save and load update

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -26,10 +26,19 @@ func _on_new_button_pressed() -> void:
 func _on_load_button_pressed() -> void:
 	print("click")
 	GameState.current_chapter = SaveLoad.contents_to_save.current_chapter
-	get_tree().change_scene_to_file("res://Scenes/Chapters/Chapter Interludes/chapter_screen.tscn")
+	
+	if GameState.current_chapter == 1:
+		get_tree().change_scene_to_file("res://Scenes/Chapters/Game Chapters/chapter_one.tscn")
+	elif GameState.current_chapter == 2:
+		get_tree().change_scene_to_file("res://Scenes/Chapters/Game Chapters/chapter_two.tscn")
+	elif GameState.current_chapter == 3:
+		get_tree().change_scene_to_file("res://Scenes/Chapters/Game Chapters/chapter_three.tscn")
+	elif GameState.current_chapter == 4:
+		get_tree().change_scene_to_file("res://Scenes/Chapters/Game Chapters/chapter_four.tscn")
 
 
 
 
 func _on_quit_button_pressed() -> void:
 	SaveLoad._save()
+	get_tree().quit()
